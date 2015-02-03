e05f350b5218114597d46f28433f1620fbbb2ce0
Nicolas JEUDY <nicolas@sudokeys.com>
2015-02-03 01:19:52 +0100

    fix: @typo in openerp description file

--- crm_activity_meeting/__openerp__.py
+++ crm_activity_meeting/__openerp__.py
@@ -14,9 +14,8 @@
     "auto_installable": False,
     "data": [
         'view/view.xml',
-        'view/ir_ui_view_record.xml",
+        'view/ir_ui_view_record.xml',
         'crm_activity_type_record.xml',
         'crm_activity_end_result_record.xml',
     ],
 }
-
